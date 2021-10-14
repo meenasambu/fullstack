a467102cc2efebfd54099f44e5abc3b9759a5c9a
meenasambu <sambumeena4853191@gmail.com>
2021-10-14 22:06:55 +0530

    modifications through JS & using styles &class modification

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var heading =document.getElementById('heading')
-var para=document.getElementsByClassName('para')
-var tags=document.getElementsByTagName('a')
-var query =document.querySelector('#heading')
-var query =document.querySelector('.para')
-// in query css selectors are mentioned 
-// in query it returns only para not array(in console we get html collection of arrays)
-// diff in query and para is returning an para and a array
-console.log(tags)
-console.log(query)
-console.log(para)
-//
-var query =document.querySelectorAll('.para')
-console.log(query)
+var heading=document.getElementById('heading')
+
+console.log(heading)
+
+// we can dynamically modify content by using innerHTML & style 
+heading.innerHTML="i was added by JS"
+
+
+heading.style="color:red;font-size:2rem"
+
+//we can check wt clss does a element has
+//eg:we can also check wt classes does heading have 
+console.log(heading.classList)
+
+//to add or remove any class from an element
+heading.classList.add('five')
+heading.classList.remove('two')
+
+console.log(heading.classList)
