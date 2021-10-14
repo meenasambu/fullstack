7260d24db260da02eb81e30e7a48ea04d7b68055
meenasambu <sambumeena4853191@gmail.com>
2021-10-14 15:29:20 +0530

    diff types of assigning a value

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-var age 
-console.log(Name)
+// compiler compiles line by line
+
+
+var arr=[1,2,3,4,"age"]
+
+
+
+console.log(arr)
+arr.push(5)
+console.log(arr.length)
+console.log(typeof arr)
+
+// as compilier compiles sep by step 
+// last most assigned number will be the value of var
+var age = 19
+age=21
+console.log(age)
+
+// const is used , not to alter the assigned value,
+// it throws an error bcoz sim has already assigned some value
+const sim=19
+sim=21
+console.log(age)
+
+
