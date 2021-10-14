bb49706cfc39a481a5d0428adc0fef7ca0fad65c
meenasambu <sambumeena4853191@gmail.com>
2021-10-14 23:24:42 +0530

    basics

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 var heading=document.getElementById('heading')
+var body=document.querySelector('body')
 
-console.log(heading)
 
-// we can dynamically modify content by using innerHTML & style 
 heading.innerHTML="i was added by JS"
-
-
 heading.style="color:red;font-size:2rem"
 
-//we can check wt clss does a element has
-//eg:we can also check wt classes does heading have 
-console.log(heading.classList)
 
-//to add or remove any class from an element
 heading.classList.add('five')
 heading.classList.remove('two')
 
 console.log(heading.classList)
+
+//dynamically adding class dark and pre-defined styles of dark in HTML doc
+body.classList.add('dark')
