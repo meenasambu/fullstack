c5c84038db1546941823997bae3afbe328e8c42b
meenasambu <sambumeena4853191@gmail.com>
2021-10-15 00:45:46 +0530

    for loop & append child

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-var heading=document.getElementById('heading')
-var body=document.querySelector('body')
+var list = document.querySelector('ul')
+
+console.log(list)
+
+var arr=['Go to the gym','cook oatmeal','eat']
+
+for (var i=0; i<arr.length;i++){
+    var element=document.createElement('li')
+    var textnode=document.createTextNode(arr[i])
+    console.log(textnode)
+    element.appendChild(textnode)
+    list.appendChild(element)
+    console.log(textnode)
+}
 
 
-heading.innerHTML="i was added by JS"
-heading.style="color:red;font-size:2rem"
 
 
-heading.classList.add('five')
-heading.classList.remove('two')
 
-console.log(heading.classList)
 
-//dynamically adding class dark and pre-defined styles of dark in HTML doc
-body.classList.add('dark')
+
+// to iterate all elements in the list in html doc
+//we need two nodes in DOM , one for ul and other for elements li
+//in DOM we have head & body , rn we are only concerned abt body
+//Body-->ul-->li-->text
+//         -->li-->text
+//         -->li-->text
+//appending text to li node then iterating li in ul node
