51dd5679bde1f4bd93d05abae9cecbec0f7734cc
meenasambu <sambumeena4853191@gmail.com>
2021-10-14 15:15:13 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-// string 
-console.log(typeof "hello world")
-console.log(typeof "d")
-
-// numbers
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 1112256597765544443234178888888888)
-console.log(typeof -1)
-console.log(typeof -1.2)
-console.log(typeof 112345668779086544424433176286)
-
-// boolean
-console.log(true)
-console.log(false)
-console.log(typeof true)
-console.log(typeof false)
+var age 
+console.log(Name)
