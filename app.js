1a7afd9e2d37140a4c6225352983be2d1d4a557a
meenasambu <sambumeena4853191@gmail.com>
2021-10-15 22:03:33 +0530

    dark-mode-button

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
+var body = document.querySelector('body')
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
 
 const callbackfunc =(event) => {
-    //console.log(button.value)  // gives the value entered in the button
-    console.log(input.innerHTML)
-//     const inputvalue=input.value
-//     const element=document.createElement('li')
-//     const textnode = document.createTextNode(inputvalue)
-//     element.appendChild(textnode)
-//     list.appendChild(element)
+    body.classList.toggle('dark')
 }
-
 button.addEventListener('click',callbackfunc)
-// button.addEventListener('click',() => {
-//     console.log('sdf')
-// })
