08bafd2533437aab52cb9e1f04932f5a945d6c14
meenasambu <sambumeena4853191@gmail.com>
2021-10-15 18:36:41 +0530

    arrow function

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-// Functions
-function greet(name,age){
-    console.log(name,age)
-    console.log('Hello')
-}
-greet('ms',21)
-
-var add = function sum(a=10,b=20) {
-    var sum=a+b
-    return sum
-}
-console.log(add(10,20))
-
-// arrow function
+// ARROW FUNCTION 
 // it removes the need of using function keyword in functions
 
 var add= (a=10,b=25) => {
@@ -20,3 +7,19 @@ var add= (a=10,b=25) => {
 }
 
 console.log(add(10,30))
+
+// arrow function can be used like this as well 
+
+var sum=(a=5,b=65) => a+b
+console.log(sum(5,65))
+
+// if we have only one parameter , we can remove paranthesis as well
+// return can also be removed
+var sum=(String) => {
+    return 'hi'+String
+}
+console.log(sum(' ms'))
+
+var sum = String => 'hi ' +String
+
+console.log(sum('ms'))
