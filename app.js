c0512431e0c29d31fa6818a13aef97b46edbd46a
meenasambu <sambumeena4853191@gmail.com>
2021-10-18 01:06:28 +0530

    var vs let

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var arr = [1,2,3,4,5,6,7,8]
+if (true) {
+    let name1 = 'rangerower'
+    
+}
+console.log(name1)
 
-arr.splice(3,1)
-console.log(arr)
-
-arr.splice(3,3)
-console.log(arr)
-
-// deletes the element of the mentioned index 
-//and no.of elements from that mentioned index.
+if (true) {
+    var name = 'ferrari'
+    
+}
+console.log(name)
