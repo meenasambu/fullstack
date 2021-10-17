90f1e98b3652e43bd8682c674c411ee0fdd706c7
meenasambu <sambumeena4853191@gmail.com>
2021-10-17 15:33:01 +0530

    calculator

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
 
-const callbackfunc =(event) => {
-    body.classList.toggle('dark')
+const add=document.querySelector('#add')
+const multiply=document.querySelector('#multiply')
+const divide = document.querySelector('#divide')
+const subtract=document.querySelector('#subtract')
+
+const resultbox=document.querySelector('.result')
+
+const addfunc=() => {
+    const result=parseInt(a.value)+parseInt(b.value)
+    resultbox.innerHTML=result
+    console.log(result)
 }
-button.addEventListener('click',callbackfunc)
+add.addEventListener('click',addfunc)
+
+multiply.addEventListener('click',() => {
+    const result=parseInt(a.value) * parseInt(b.value)
+    resultbox.innerHTML=result
+    console.log(result)
+})
+
+divide.addEventListener('click',() => {
+    const result=parseInt(a.value) / parseInt(b.value)
+    resultbox.innerHTML=result
+    console.log(result)
+})
+
+subtract.addEventListener('click',() => {
+    const result=parseInt(a.value) - parseInt(b.value)
+    resultbox.innerHTML=result
+    console.log(result)
+    
+})
+
+
+
+
+
+
+
