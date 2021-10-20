27f7d934f54f686556e98837db60a4b70cd83b8d
meenasambu <sambumeena4853191@gmail.com>
2021-10-20 15:10:18 +0530

    added dark mode and some allignments

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
 var body = document.querySelector('body')
 var input = document.querySelector('input')
-var button =document.querySelector('button')
-
-const callbackfunc =(event) =>{
-    console.log(input.innerHTML)
-    body.classList.toggle('dark')
-}
-
-button.addEventListener('click',callbackfunc)
