f5ae2c99038c9b85c1450d45526a121c985d6fe7
meenasambu <sambumeena4853191@gmail.com>
2021-10-24 16:34:46 +0530

    jumbled table

--- app.js
+++ app.js
@@ -13,11 +13,41 @@ fetch('https://jsonplaceholder.typicode.com/users')
                  let newrow=document.createElement('tr')
                  let dataid = document.createElement('td')
                  let dataidtextnode=document.createTextNode(user.id)
-                 //in the data wt we have fetch , to access the data user.id we use
                  dataid.appendChild(dataidtextnode)
                  newrow.appendChild(dataid)
 
+                 let newrow1=document.createElement('tr')
+                 let dataname = document.createElement('td')
+                 let datanametextnode=document.createTextNode(user.name)
+                 dataid.appendChild(datanametextnode)
+                 newrow1.appendChild(dataname)
+
+                 let newrow2=document.createElement('tr')
+                 let datausername = document.createElement('td')
+                 let datausernametextnode=document.createTextNode(user.username)
+                 datausername.appendChild(datausernametextnode)
+                 newrow2.appendChild(datausername)
+
+                 let newrow3=document.createElement('tr')
+                 let dataemail= document.createElement('td')
+                 let dataemailtextnode=document.createTextNode(user.email)
+                 dataemail.appendChild(dataemailtextnode)
+                 newrow3.appendChild(dataemail)
+
+                 let newrow4=document.createElement('tr')
+                 let datahno = document.createElement('td')
+                 let datahnotextnode=document.createTextNode(user.address)
+                 datahno.appendChild(datahnotextnode)
+                 newrow4.appendChild(datahno)
+
+
+
                  table.appendChild(newrow)
+                 table.appendChild(newrow1)
+                 table.appendChild(newrow2)
+                 table.appendChild(newrow3)
+                 table.appendChild(newrow4)
+
 
              })
         })
