de24309b105446e62c895fa45152f5b613950bdf
meenasambu <sambumeena4853191@gmail.com>
2021-10-25 10:20:48 +0530

    creating table from url

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const table = document.querySelector('table')
 console.log(table)
 
 
+const createRow=(user)=> {
+    let newRow=document.createElement('tr')
+    let element = `
+    <td>${user.id}</td>
+    <td>${user.name}</td>
+    <td>${user.email}</td>
+    <td>${user.username}</td>`
+    newRow.innerHTML = element
+    return newRow
 
+}
 
 fetch('https://jsonplaceholder.typicode.com/users')
          .then(res =>res.json())
@@ -10,20 +20,12 @@ fetch('https://jsonplaceholder.typicode.com/users')
              console.log(data)
              data.forEach(user => {
                  
-                 let newRow=document.createElement('tr')
-                 let element = `
-                 <td>${user.id}</td>
-                 <td>${user.name}</td>
-                 <td>${user.email}</td>
-                 <td>${user.username}</td>`
-                 newRow.innerHTML = element
-                 table.appendChild(newRow)
+               
+                 table.appendChild(createRow(user))
              })
-        })
-
-
-        
+        })     
 // last time we did same thing for string 
 // but string data type is diff from node 
 // appendchild accepts only node
+
 
